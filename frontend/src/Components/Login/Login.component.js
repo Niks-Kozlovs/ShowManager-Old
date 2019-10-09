543bf6b9d5e2bd24a26d8ae9ef93230ff3a06938
Niks Kozlovs <kozlovs.niks1@gmail.com>
2019-10-09 16:58:21 +0300

    MD-3 Improved navbar styling and elements

--- frontend/src/Components/Login/Login.component.js
+++ frontend/src/Components/Login/Login.component.js
@@ -9,11 +9,10 @@ class Login extends Component {
 
     render() {
         return (
-            <div
+            <button
               className="Login"
               onClick={ () => this.login() }
               onKeyPress={ () => this.login() }
-              role="button"
               tabIndex="0"
             >
                 <svg>
@@ -21,7 +20,7 @@ class Login extends Component {
                     <path id="login" d="M17.582 17.582a6.791 6.791 0 1 0-6.791-6.791 6.789 6.789 0 0 0 6.791 6.791zm0 3.4C13.049 20.977 4 23.252 4 27.768v3.4h27.164v-3.4c0-4.516-9.049-6.791-13.582-6.791z" transform="translate(-4 -4)" />
                 </svg>
                 <p>Login</p>
-            </div>
+            </button>
         );
     }
 }
